0cc6bc1ba9b3c9a1c41c5c6bef28fe538d38b4cc
Carolina Moore <caroavello.m@gmail.com>
2023-03-07 17:35:16 -0300

    simple counter

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+
+const App = (props) => {
+
+  return (
+    <>
+      <div className='d-flex flex-row justify-content-center bg-black' id='timer'>
+            <i className="m-2 p-3 mb-2 bg-dark text-white"></i>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num1 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num2 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num3 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num4 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num5 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num6 % 10}</p>
+        </div>
+    </>
+  );
+};
+
+export default App;
